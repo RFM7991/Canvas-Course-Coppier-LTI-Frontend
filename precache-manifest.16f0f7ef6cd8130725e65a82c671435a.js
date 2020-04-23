bc708b0e4974c2feedf07f4a4c95c1cbbcd74d60
Robert Francisco McPherson <mcphersonr1@DIT-02SJ0WSG8WM.montclair.edu>
2020-04-23 10:51:30 -0400

    Updates

---
+++ precache-manifest.16f0f7ef6cd8130725e65a82c671435a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ed67b9b98e7095eea42b6cd14aa2d3",
+    "url": "/Canvas-Course-Coppier-LTI-Frontend/index.html"
+  },
+  {
+    "revision": "48c76f296358b2b9c621",
+    "url": "/Canvas-Course-Coppier-LTI-Frontend/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "b191a62e0fcda087f232",
+    "url": "/Canvas-Course-Coppier-LTI-Frontend/static/css/main.f1dae679.chunk.css"
+  },
+  {
+    "revision": "48c76f296358b2b9c621",
+    "url": "/Canvas-Course-Coppier-LTI-Frontend/static/js/2.bbbdbf45.chunk.js"
+  },
+  {
+    "revision": "b191a62e0fcda087f232",
+    "url": "/Canvas-Course-Coppier-LTI-Frontend/static/js/main.39d60a09.chunk.js"
+  },
+  {
+    "revision": "821c037a0247e8e32387",
+    "url": "/Canvas-Course-Coppier-LTI-Frontend/static/js/runtime~main.a08bdf65.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Canvas-Course-Coppier-LTI-Frontend/precache-manifest.16f0f7ef6cd8130725e65a82c671435a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Canvas-Course-Coppier-LTI-Frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
